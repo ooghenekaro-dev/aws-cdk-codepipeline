5d9e54d5676f842e93d99696561c73df9c635a33
ooghenekaro <oghenekaro.oboido@unicard-uk.com>
2025-06-06 07:51:31 +0100

    initial commit

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,12 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new CodePipeline(this, 'Pipeline', {
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('aws-samples/aws-cdk-pipelines', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
 
     // example resource
     // const queue = new sqs.Queue(this, 'CdkPipelineQueue', {
