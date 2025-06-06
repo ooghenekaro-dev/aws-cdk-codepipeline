4ccf7f79cf867fe44a9ce4608e5158db42af44c0
ooghenekaro <oghenekaro.oboido@unicard-uk.com>
2025-06-06 08:36:24 +0100

    give pipeline a name

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -8,6 +8,7 @@ export class CdkPipelineStack extends cdk.Stack {
     super(scope, id, props);
 
     new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'GuguruPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.connection('ooghenekaro-dev/aws-cdk-codepipeline', 'main', {
           connectionArn: 'arn:aws:codeconnections:eu-west-2:233535120968:connection/a87a8ab2-a00d-43ba-bdd9-b7978f3db375' // Created using the AWS console
