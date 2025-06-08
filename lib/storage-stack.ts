16672c40764c34f374c3424fd0534b896f15f24a
ooghenekaro <oghenekaro.oboido@unicard-uk.com>
2025-06-08 06:25:41 +0100

    make bucket name uniquely global

--- lib/storage-stack.ts
+++ lib/storage-stack.ts
@@ -1,16 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import { env } from 'process';
 
 export class storageStack extends cdk.Stack {
   constructor(scope: Construct, id: string, StageName: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     new s3.Bucket(this, 'bucket', {
-      bucketName: 'guguru-kuvuki-bucket',
+      bucketName: `guguru-kuvuki-bucket-${StageName.toLowerCase()}`,
       versioned: true,
-      encryption: s3.BucketEncryption.S3_MANAGED
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
     });
 
+
   }
+
 }
